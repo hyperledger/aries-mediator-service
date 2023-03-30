df1a0dfb8e0d8931977fb60fe56d5f6d8cf3f6b0
Kim Ebert <kim@indicio.tech>
2023-03-30 10:46:40 -0600

    feat: add additional load tests
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- load-testing/load-agent/agent.js
+++ load-testing/load-agent/agent.js
@@ -19,31 +19,40 @@ function generateString(length) {
     return result;
 }
 
-const initializeAgent = async () => {
+const initializeAgent = async (withMediation, port) => {
     // Simple agent configuration. This sets some basic fields like the wallet
     // configuration and the label. It also sets the mediator invitation url,
     // because this is most likely required in a mobile environment.
 
+    let mediation_url = config.mediation_url;
+    let endpoints = ['http://'+ config.agent_ip + ':' + port] 
+
     const agentConfig = {
+        indyLedgers: [config.ledger],
         label: generateString(14),
         walletConfig: {
             id: generateString(32),
             key: generateString(32),
         },
         autoAcceptConnections: true,
-        // endpoints: ['http://localhost:3002'],
+        endpoints: endpoints,
 
-        // autoAcceptInvitation: true,
+        autoAcceptInvitation: true,
         // logger: new ariesCore.ConsoleLogger(ariesCore.LogLevel.trace),
-        mediatorConnectionsInvite: config.mediation_url,
+        mediatorConnectionsInvite: mediation_url,
+    }
+
+    if (withMediation) {
+        delete agentConfig['endpoints']
+    } else {
+        delete agentConfig['mediatorConnectionsInvite']
     }
 
     // A new instance of an agent is created here
-    const agent = new ariesCore.Agent(
-        {
+    const agent = new ariesCore.Agent({
             config: agentConfig, 
-            dependencies: ariesNode.agentDependencies}
-        )
+            dependencies: ariesNode.agentDependencies
+        })
 
     // Register a simple `WebSocket` outbound transport
     agent.registerOutboundTransport(new ariesCore.WsOutboundTransport())
@@ -51,38 +60,43 @@ const initializeAgent = async () => {
     // Register a simple `Http` outbound transport
     agent.registerOutboundTransport(new ariesCore.HttpOutboundTransport())
 
-    // agent.registerInboundTransport(new ariesNode.HttpInboundTransport({ port: 3002 }))
 
-    // wait for medation to be configured
-    let timeout = 2 * 60000 ;// two minutes 
+    if (withMediation) {
 
-    const TimeDelay = new Promise((resolve, reject) => {
-        setTimeout(resolve, timeout, false)
-    })
+        // wait for medation to be configured
+        let timeout = 2 * 60000 ;// two minutes 
 
-    var def = deferred()
+        const TimeDelay = new Promise((resolve, reject) => {
+            setTimeout(resolve, timeout, false)
+        })
 
-    var onConnectedMediation = async (event)=>{
-        const mediatorConnection = await agent.mediationRecipient.findDefaultMediatorConnection()
-        if(event.payload.connectionId === mediatorConnection?.id){
-            def.resolve(true)
-            // we no longer need to listen to the event
-            agent.events.off(ariesCore.TransportEventTypes.OutboundWebSocketOpenedEvent, onConnectedMediation);
+        var def = deferred()
+
+        var onConnectedMediation = async (event)=>{
+            const mediatorConnection = await agent.mediationRecipient.findDefaultMediatorConnection()
+            if(event.payload.connectionId === mediatorConnection?.id){
+                def.resolve(true)
+                // we no longer need to listen to the event
+                agent.events.off(ariesCore.TransportEventTypes.OutboundWebSocketOpenedEvent, onConnectedMediation);
+            }
         }
-    }
 
-    agent.events.on(ariesCore.TransportEventTypes.OutboundWebSocketOpenedEvent, onConnectedMediation)
+        agent.events.on(ariesCore.TransportEventTypes.OutboundWebSocketOpenedEvent, onConnectedMediation)
 
-    // Initialize the agent
-    await agent.initialize()
+        // Initialize the agent
+        await agent.initialize()
 
-    // wait for ws to be configured
-    value = await Promise.race([TimeDelay, def.promise])
+        // wait for ws to be configured
+        value = await Promise.race([TimeDelay, def.promise])
 
-    if (!value) {
-        // we no longer need to listen to the event in case of failure
-        agent.events.off(ariesCore.TransportEventTypes.OutboundWebSocketOpenedEvent, onConnectedMediation);
-        throw 'Mediator timeout!';
+        if (!value) {
+            // we no longer need to listen to the event in case of failure
+            agent.events.off(ariesCore.TransportEventTypes.OutboundWebSocketOpenedEvent, onConnectedMediation);
+            throw 'Mediator timeout!';
+        }
+    } else {
+        agent.registerInboundTransport(new ariesNode.HttpInboundTransport({ port: port }))
+        await agent.initialize()
     }
 
     return agent
@@ -103,9 +117,10 @@ const pingMediator = async(agent) => {
     var onPingResponse = async (event)=>{
         const mediatorConnection = await agent.mediationRecipient.findDefaultMediatorConnection()
         if(event.payload.connectionRecord.id === mediatorConnection?.id){
-            def.resolve(true)
             // we no longer need to listen to the event
             agent.events.off(ariesCore.TrustPingEventTypes.TrustPingResponseReceivedEvent, onPingResponse);
+
+            def.resolve(true)
         }
 
     }
@@ -129,7 +144,128 @@ const pingMediator = async(agent) => {
     }
 }
 
+let receiveInvitation = async(agent, invitationUrl) => {
+
+    // wait for the connection
+    let timeout = 2 * 60000 ;// two minutes 
+
+    const TimeDelay = new Promise((resolve, reject) => {
+        setTimeout(resolve, timeout, false)
+    })
+
+    var def = deferred()
+
+    var onConnection = async (event) => {
+        {
+            let payload = event.payload
+            if (payload.connectionRecord.state === ariesCore.DidExchangeState.Completed) {
+                // the connection is now ready for usage in other protocols!
+                // console.log(`Connection for out-of-band id ${payload.connectionRecord.outOfBandId} completed`)
+                // Custom business logic can be included here
+                // In this example we can send a basic message to the connection, but
+                // anything is possible
+
+                agent.events.off(ariesCore.ConnectionEventTypes.ConnectionStateChanged, onConnection);
+
+                def.resolve(true)
+            }
+        }
+    }
+
+    agent.events.on(ariesCore.ConnectionEventTypes.ConnectionStateChanged, onConnection)
+
+    const { outOfBandRecord } = await agent.oob.receiveInvitationFromUrl(invitationUrl)
+
+    // wait for connection
+    value = await Promise.race([TimeDelay, def.promise])
+
+    if (!value) {
+        // we no longer need to listen to the event in case of failure
+        agent.events.off(ariesCore.ConnectionEventTypes.ConnectionStateChanged, onConnection);
+        throw 'Connection timeout!';
+    }
+
+    return outOfBandRecord
+}
+
+let receiveCredential = async (agent) => {
+    // wait for the ping
+    let timeout = 2 * 60000 ;// two minutes 
+
+    const TimeDelay = new Promise((resolve, reject) => {
+        setTimeout(resolve, timeout, false)
+    })
+
+    var def = deferred()
+
+    let onCredential = async (event) => {
+        let payload = event.payload
+
+        switch (payload.credentialRecord.state) {
+            case ariesCore.CredentialState.OfferReceived:
+            //console.log('received a credential')
+            // custom logic here
+            await agent.credentials.acceptOffer({ credentialRecordId: payload.credentialRecord.id })
+            case ariesCore.CredentialState.Done:
+            //console.log(`Credential for credential id ${payload.credentialRecord.id} is accepted`)
+            // For demo purposes we exit the program here.
+
+            agent.events.off(ariesCore.CredentialEventTypes.CredentialStateChanged, onCredential);
+
+            def.resolve(true)
+        }
+    }
+
+    agent.events.on(ariesCore.CredentialEventTypes.CredentialStateChanged, onCredential)
+
+    // Nothing for us to do
+
+    // wait for credential
+    value = await Promise.race([TimeDelay, def.promise])
+
+    if (!value) {
+        // we no longer need to listen to the event in case of failure
+        agent.events.off(ariesCore.CredentialEventTypes.CredentialStateChanged, onCredential);
+        throw 'Credential timeout!';
+    }
+}
+
+let receiveMessage = async(agent) => {
+    // wait for the ping
+    let timeout = 2 * 60000 ;// two minutes 
+
+    const TimeDelay = new Promise((resolve, reject) => {
+        setTimeout(resolve, timeout, false)
+    })
+
+    var def = deferred()
+
+    let onMessage = async (event) => {
+        let payload = event.payload
+
+//        console.error(payload)
+
+        agent.events.off(ariesCore.BasicMessageEventTypes.BasicMessageStateChanged, onMessage);
+
+        def.resolve(true)
+    }
+
+    agent.events.on(ariesCore.BasicMessageEventTypes.BasicMessageStateChanged, onMessage)
+
+    // Nothing for us to do
+
+    // wait for credential
+    value = await Promise.race([TimeDelay, def.promise])
+
+    if (!value) {
+        // we no longer need to listen to the event in case of failure
+        agent.events.off(ariesCore.BasicMessageEventTypes.BasicMessageStateChanged, onMessage);
+        throw 'Message timeout!';
+    } 
+}
+
 var readline = require('readline');
+const { ConsoleLogger } = require('@aries-framework/core')
 
 var rl = readline.createInterface(
     process.stdin, null);
@@ -150,16 +286,30 @@ rl.on('line', async (line) => {
 
         if ( command['cmd'] == 'start' && agent == null) {
         
-            agent = await initializeAgent()
+            agent = await initializeAgent(command['withMediation'], command['port'])
             process.stdout.write(JSON.stringify({'error':0, 'result': 'Initialized agent...'}) + "\n")
         
         } else if ( command['cmd'] == 'ping_mediator') {
             await pingMediator(agent)
 
             process.stdout.write(JSON.stringify({'error':0, 'result': 'Ping Mediator'}) + "\n")
+        } else if ( command['cmd'] =='receiveInvitation') {
+            let connection = await receiveInvitation(agent, command['invitationUrl'])
+
+            process.stdout.write(JSON.stringify({'error':0, 'result': 'Receive Connection', "connection": connection}) + "\n")
+        } else if ( command['cmd'] =='receiveCredential') {
+            await receiveCredential(agent)
+
+            process.stdout.write(JSON.stringify({'error':0, 'result': 'Receive Credential'}) + "\n")
+        } else if ( command['cmd'] == 'receiveMessage') {
+            await receiveMessage(agent)
+
+            process.stdout.write(JSON.stringify({'error':0, 'result': 'Receive Message'}) + "\n")
         } else if ( command['cmd'] =='shutdown') {
             process.stdout.write(JSON.stringify({'error':0, 'result': 'Shutting down agent'}) + "\n")
             rl.close();
+            await agent.shutdown();
+            process.exit(1);
         } else {
             handleError('invalid command')
         }
@@ -178,8 +328,10 @@ process.once('SIGTERM', function (code) {
 process
   .on('unhandledRejection', (reason, p) => {
     handleError(reason);
+    process.exit(1);
   })
   .on('uncaughtException', err => {
     handleError(err);
     process.exit(1);
   });
+  
---
+++ load-testing/load-agent/locustClient.py
@@ -0,0 +1,297 @@
+from locust import events
+import time
+import inspect
+import json
+
+import fcntl
+import os
+import requests
+import signal
+
+from gevent import subprocess
+from gevent import select
+from gevent import lock as gevent_lock
+
+SHUTDOWN_TIMEOUT_SECONDS=10
+READ_TIMEOUT_SECONDS=120
+ERRORS_BEFORE_RESTART=10
+START_PORT= json.loads(os.getenv('START_PORT'))
+END_PORT= json.loads(os.getenv('END_PORT'))
+class PortManager:
+    def __init__(self):
+        self.lock = gevent_lock.BoundedSemaphore()
+        self.ports = list(range(START_PORT,END_PORT))
+
+    def getPort(self):
+        self.lock.acquire()
+        try:
+            port = self.ports.pop(0)
+            return port
+        finally:
+            self.lock.release()
+
+    def returnPort(self, port):
+        self.lock.acquire()
+        try:
+            self.ports.append(port)
+        finally:
+            self.lock.release()
+
+portmanager = PortManager()
+
+def stopwatch(func):
+    def wrapper(*args, **kwargs):
+        # get task's function name
+        previous_frame = inspect.currentframe().f_back
+        file_name, _, task_name, _, _ = inspect.getframeinfo(previous_frame)
+
+        start = time.time()
+        result = None
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            total = int((time.time() - start) * 1000)
+            events.request_failure.fire(request_type="TYPE",
+                                        name=file_name + '_' + task_name,
+                                        response_time=total,
+                                        exception=e,
+                                        response_length=0)
+        else:
+            total = int((time.time() - start) * 1000)
+            events.request_success.fire(request_type="TYPE",
+                                        name=file_name + '_' + task_name,
+                                        response_time=total,
+                                        response_length=0)
+        return result
+
+    return wrapper
+
+class CustomClient:
+    def __init__(self, host):
+        self.host = host
+        self.agent = None
+        self.errors = 0
+        self.port = None
+        self.withMediation = None
+
+    _locust_environment = None
+
+    @stopwatch
+    def startup(self, withMediation=True):
+        if (not self.withMediation) and self.withMediation is None:
+            self.withMediation = withMediation
+        try:
+            if self.port is not None:
+                portmanager.returnPort(self.port)
+                self.port = None
+            self.port = portmanager.getPort()
+
+            self.errors = 0
+            self.agent = subprocess.Popen(['node', 'agent.js'], 
+                bufsize=0,
+                universal_newlines=True, 
+                stdout=subprocess.PIPE, 
+                stdin=subprocess.PIPE,
+                shell=False)
+
+            self.run_command({
+                "cmd": "start", 
+                "withMediation": self.withMediation,
+                "port": self.port
+            })
+
+            line = self.readjsonline()
+
+            # we tried to start the agent and failed
+            if self.agent is None or self.agent.poll() is not None: 
+                raise Exception('unable to start')
+        except Exception as e:
+            self.shutdown()
+            raise e
+        
+    def shutdown(self):
+        # Read output until process closes
+
+        try:
+            if self.port:
+                portmanager.returnPort(self.port)
+                self.port = None
+
+            # We write the command by hand here because if we call the cmd function
+            # above we could end up in an infinite loop on shutdown
+            self.agent.stdin.write(json.dumps({"cmd":"shutdown"}))
+            self.agent.stdin.write("\n")
+            self.agent.stdin.flush()
+
+            self.agent.communicate(timeout=SHUTDOWN_TIMEOUT_SECONDS)
+        except Exception as e:
+            pass
+        finally:
+            try:
+                os.kill(self.agent.pid, signal.SIGTERM)
+            except Exception as e:
+                pass
+            self.agent = None
+
+    def ensure_is_running(self):
+        # Is the agent started?
+        if not self.agent:
+            self.startup()
+        
+        # is the agent still running?
+        elif self.agent.poll() is None:
+            # check for closed Pipes
+            if self.agent.stdout.closed or self.agent.stdin.closed:
+                self.startup()
+            else:
+                return True
+        else:
+            self.startup()
+
+    def is_running(self):
+        if self.agent:
+            return True
+        return False
+
+    def run_command(self, command):
+        try:
+            self.agent.stdin.write(json.dumps(command))
+            self.agent.stdin.write("\n")
+            self.agent.stdin.flush()
+        except Exception as e:
+            # if we get an exception here, we cannot run any new commands
+            self.shutdown()
+            raise e
+
+
+    def readjsonline(self):
+        try:
+            line = None
+
+            if not self.agent.stdout.closed:
+                q = select.poll()
+                q.register(self.agent.stdout,select.POLLIN)
+
+                if q.poll(READ_TIMEOUT_SECONDS * 1000):
+                    line = json.loads(self.agent.stdout.readline())
+                else:
+                    raise Exception("Read Timeout")
+
+            if not line:
+                raise Exception("invalid read")
+
+            if line['error'] != 0:
+                raise Exception(line['result'])
+
+            return line
+        except Exception as e:
+            self.errors += 1
+            if self.errors > ERRORS_BEFORE_RESTART:
+                self.shutdown() ## if we are in bad state we may need to restart...
+            raise e
+
+    @stopwatch
+    def ping_mediator(self):
+        self.run_command({"cmd":"ping_mediator"})
+
+        line = self.readjsonline()
+
+    @stopwatch
+    def issuer_getinvite(self):
+        headers = json.loads(os.getenv('ISSUER_HEADERS'))
+        headers['Content-Type'] = 'application/json'
+        r = requests.post(
+            os.getenv('ISSUER_URL') + '/connections/create-invitation?auto_accept=true', 
+            json={ "metadata": {}, "my_label": "Test" },
+            headers=headers
+            )
+        if r.status_code != 200:
+            raise Exception(r.content)
+            
+        r = r.json()
+
+        return r
+
+    @stopwatch
+    def issuer_getliveness(self):
+        headers = json.loads(os.getenv('ISSUER_HEADERS'))
+        headers['Content-Type'] = 'application/json'
+        r = requests.get(
+            os.getenv('ISSUER_URL') + '/status', 
+            json={ "metadata": {}, "my_label": "Test" },
+            headers=headers
+            )
+        if r.status_code != 200:
+            raise Exception(r.content)
+
+        r = r.json()
+
+        return r
+
+    @stopwatch
+    def accept_invite(self, invite):
+        self.run_command({"cmd":"receiveInvitation", "invitationUrl": invite})
+
+        line = self.readjsonline()
+
+        return line['connection']
+
+    @stopwatch
+    def receive_credential(self, connection_id):
+        self.run_command({"cmd":"receiveCredential"})
+
+        headers = json.loads(os.getenv('ISSUER_HEADERS'))
+        headers['Content-Type'] = 'application/json'
+
+        issuer_did = os.getenv('CRED_DEF').split(':')[0]
+        schema_parts = os.getenv('SCHEMA').split(':')
+
+
+        r = requests.post(
+            os.getenv('ISSUER_URL') + '/issue-credential/send', 
+            json={
+                "auto_remove": True,
+                "comment": "Performance Issuance",
+                "connection_id": connection_id,
+                "cred_def_id": os.getenv('CRED_DEF'),
+                "credential_proposal": {
+                    "@type": "issue-credential/1.0/credential-preview",
+                    "attributes": json.loads(os.getenv('CRED_ATTR'))
+                },
+                "issuer_did": issuer_did,
+                "schema_id":  os.getenv('SCHEMA'),
+                "schema_issuer_did": schema_parts[0],
+                "schema_name": schema_parts[2],
+                "schema_version": schema_parts[3],
+                "trace": True
+            },
+            headers=headers
+            )
+        if r.status_code != 200:
+            raise Exception(r.content)
+            
+        r = r.json()
+
+        line = self.readjsonline()
+
+        return line
+
+    @stopwatch
+    def msg_client(self, connection_id):
+        self.run_command({"cmd":"receiveMessage"})
+
+        headers = json.loads(os.getenv('ISSUER_HEADERS'))
+        headers['Content-Type'] = 'application/json'
+
+        r = requests.post(
+            os.getenv('ISSUER_URL') + '/connections/' + connection_id + '/send-message', 
+            json={
+                "content": "ping"
+            },
+            headers=headers
+            )
+        r = r.json()
+
+        line = self.readjsonline()
+
+        return line
